86d090a9ae337bb02f1ea34dd86f5ef885b7518c
Duran Sakalli <45287656+duransakalli@users.noreply.github.com>
2020-12-24 22:19:38 -0800

    first commit

---
+++ spring-boot-qr-code-generator-app/src/main/java/duran/springbootqrcodegeneratorapp/SpringBootQrCodeGeneratorAppApplication.java
@@ -0,0 +1,13 @@
+package duran.springbootqrcodegeneratorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGeneratorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQrCodeGeneratorAppApplication.class, args);
+    }
+
+}
---
+++ spring-boot-qr-code-generator-app/src/test/java/duran/springbootqrcodegeneratorapp/SpringBootQrCodeGeneratorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package duran.springbootqrcodegeneratorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGeneratorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
